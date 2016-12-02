41209aa59a1066d4e8cfe6023feac781814a612a
Jeff Kealy <jeffkealy@yahoo.com>
2016-12-02 00:07:42 -0600

    first commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,21 @@
-var app = angular.module('app', []);
+var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
+ console.log("myApp!");
 
-app.controller('firstController', function() {
-  console.log('firstController up and running');
-  var self = this;
-  self.message = "Welcome!"
-});
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/jeff', {
+            templateUrl: '/views/templates/jeff.html',
+            controller: 'JeffController',
+            controllerAs: 'jc'
+        })
+        .otherwise({
+            redirectTo: '/jeff'
+        });
+
+// app.controller('firstController', function() {
+//   console.log('firstController up and running');
+//   var self = this;
+//   self.message = "Welcome!"
+// });
+
+}]);
---
+++ server/public/scripts/controllers/jeff.controller.js
@@ -0,0 +1,12 @@
+app.controller('JeffController', function($mdBottomSheet){
+  console.log("JeffController running");
+  var self = this;
+  self.share = function(){
+    console.log("click works");
+    $mdBottomSheet.show({
+      templateUrl: "../../views/templates/bottomsheet.html",
+      controller: 'JeffController'
+    })
+
+  }
+});
